4c9a3662f62a9b06191c2cec8ebea315a93ec960
ShemBrooklyne <shemnyatti@gmail.com>
2020-07-16 12:02:37 +0300

    Calculator prac proj

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+/* var add = function(function1, function2) {
+return function1 + function2;
+};
+
+var subtract = function(function1, function2) {
+return function1 - function2
+};
+alert(subtract(10, 5));
+
+var add = function(function1, function2) {
+return function1 + function2
+}
+var number1 = prompt("Enter a number N*ggh:")
+alert(number1); */
+
+/* var add = function(function1, function2) {
+return function1 + function2;
+}
+var number1 = prompt("Enter a number boy!");
+var number2 = prompt("Enter another number");
+alert(add(number1, number2)); */
+
+var add = function (function1, function2) {
+    return function1 + function2;
+}
+var number1 = parseInt(prompt("Enter a number boy!"));
+var number2 = parseInt(prompt("Enter another number"));
+alert(add(number1, number2));
