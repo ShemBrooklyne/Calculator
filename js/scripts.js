6eeaac63f1eeeab29ecfb2de50f05f5d299e4afa
ShemBrooklyne <shemnyatti@gmail.com>
2020-07-16 12:24:14 +0300

    Working Calculator

--- js/scripts.js
+++ js/scripts.js
@@ -20,9 +20,39 @@ var number1 = prompt("Enter a number boy!");
 var number2 = prompt("Enter another number");
 alert(add(number1, number2)); */
 
+
+// Addition B/ness logic
 var add = function (function1, function2) {
     return function1 + function2;
 }
 var number1 = parseInt(prompt("Enter a number boy!"));
 var number2 = parseInt(prompt("Enter another number"));
 alert(add(number1, number2));
+
+// Subtraction B/ness logic
+var Subtraction = function (func1, func2) {
+    return func1 - func2;
+}
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+alert(Subtraction(number1, number2));
+
+// Multiplicattion B/ness logic
+
+var multiplication = function(entry1, entry2) {
+    return entry1 * entry2;
+}
+var number1 = parseInt(prompt("Enter number to multiply:"));
+var number2 = parseInt(prompt("Enter number to multiply it with:"));
+alert(multiplication(number1, number2));
+
+// Division B/ness logic
+
+var divide = function(div1, div2) {
+    return div1/div2;
+}
+var number1 = parseInt(prompt("For division, enter a number:"));
+var number2 = parseInt(prompt("Enter number to divide with:"));
+alert(divide(number1, number2))
+
+
